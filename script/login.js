6cadfff3b17b216f4a06dc6095dd335b767b6aa5
guaguagua000 <1484522055@qq.com>
2019-10-13 15:27:24 +0800

    add frontend

---
+++ script/login.js
@@ -0,0 +1,81 @@
+let loginBox = document.querySelector("#login-wrapper");
+let userLogin = document.querySelector("#user-login");
+let userName = document.querySelector("#user-name");
+let userPassword = document.querySelector("#user-pwd");
+let userSubmit = document.querySelector("#user-submit");
+let checkMsg = document.querySelector("#check-msg");
+let flag = 0;
+
+window.onload = setFocus(userName);
+
+console.log(window.localStorage["isLogin"]);
+
+userSubmit.addEventListener("click", function(e) {
+	var e = e || window.event;
+	var target = e.target || e.srcElement;
+	var name = userName.value.trim();
+	var password = userPassword.value.trim();
+	if(name === "") {
+		showMsg("用户名不能为空");
+		return ;
+	}
+	if(password === "") {
+		showMsg("密码不能为空");
+		return ;
+	}
+	postUser(name, password);
+});
+
+
+function showMsg(str) {
+	checkMsg.innerHTML = str;
+	checkMsg.classList.add("msg");
+}
+
+function setFocus(input) {
+	input.focus();
+}
+
+function postUser(name, password) {
+	var url = 'https://www.fastmock.site/mock/0ca083d3c1d3e79c2abdb96367fac9dd/api/login';
+	var requestString = "username=" + name + "&password=" + password;
+	var oAjax = null;
+
+	if(window.XMLHttpRequest) {
+		oAjax = new XMLHttpRequest();
+	} else {
+		oAjax = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	oAjax.open("POST", url, true);
+	oAjax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	oAjax.send(requestString);
+
+	oAjax.onreadystatechange = function(e) {
+		var e = e || window.event;
+		var target = e.target || e.srcElement;
+		
+		if(target.readyState === 4 && target.status === 200) {
+			var resultStr = target.responseText;
+			var resultObj = eval("(" + resultStr + ")");
+			if(resultObj.status === "1") {
+				showMsg('账号或密码错误');
+			} else {
+				localStorage.setItem("isLogin", true);
+				localStorage.setItem("userID", name);
+				localStorage.setItem("position", resultObj.position);
+				localStorage.setItem("userStatus", null);
+				switch(resultObj.position) {
+					case "0" :
+						window.location.href = "../pages/student.html";
+						break;
+					case "1" :
+						window.location.href = "../pages/teacher.html";
+						break;
+					case "2"  :
+						window.location.href = "../pages/admin.html";
+				}
+			}
+		}
+	}
+}
